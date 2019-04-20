77eb9e4920b0625057ec1580d7a882d47dd4925e
amitbarai <arbarai@hotmail.com>
2019-04-20 10:12:47 +0530

    test

--- modules/profile/manifests/hiera_test.pp
+++ modules/profile/manifests/hiera_test.pp
@@ -1,16 +1,16 @@
-# /etc/puppetlabs/code/environments/production/modules/profile/manifests/hiera_test.pp
-class profile::hiera_test (
-  Boolean             $ssl,
-  Boolean             $backups_enabled,
-  Optional[String[1]] $site_alias = undef,
-) {
-  file { '/tmp/hiera_test.txt':
-    ensure  => file,
-    content => @("END"),
-            value1  ${ssl}
-            value2  ${backups_enabled}
-               |END
-    owner   => root,
-    mode    => '0644',
-  }
-}
+## /etc/puppetlabs/code/environments/production/modules/profile/manifests/hiera_test.pp
+#class profile::hiera_test (
+#  Boolean             $ssl,
+#  Boolean             $backups_enabled,
+#  Optional[String[1]] $site_alias = undef,
+#) {
+#  file { '/tmp/hiera_test.txt':
+#    ensure  => file,
+#    content => @("END"),
+#            value1  ${ssl}
+#            value2  ${backups_enabled}
+#               |END
+#    owner   => root,
+#    mode    => '0644',
+#  }
+#}
