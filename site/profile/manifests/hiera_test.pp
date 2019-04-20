a85c40cb2c6d9c386e57db0caf7ed2dc4b1240b2
amitbarai <arbarai@hotmail.com>
2019-04-20 14:27:38 +0530

    test role profile

--- site/profile/manifests/hiera_test.pp
+++ site/profile/manifests/hiera_test.pp
@@ -1,7 +1,8 @@
 # /etc/puppetlabs/code/environments/production/modules/profile/manifests/hiera_test.pp
 class profile::hiera_test (
-  $ssl             = lookup('profile::hiera_test::ssl'),
-  $backups_enabled = lookup('profile::hiera_test::backups_enabled')
+  $ssl              = lookup('profile::hiera_test::ssl'),
+  $backups_enabled  = lookup('profile::hiera_test::backups_enabled'),
+  $env_globalcustom = lookup('tomcat::bin::setenv'),
 ) {
   file { '/tmp/hiera_test.txt':
     ensure  => file,
@@ -12,4 +13,17 @@ class profile::hiera_test (
     owner   => root,
     mode    => '0644',
   }
+  file { '/tmp/test2':
+    ensure  => 'present',
+    content => @("END"),
+    value2    ${env_globalcustom}
+              |END
+  }
+  file { '/tmp/dansfile':
+  ensure => file,
+}
+-> file_line { 'dans_line':
+  line => 'dan is awesome',
+  path => '/tmp/dansfile',
+}
 }
