b3239702e787c06752bd06ed8091a57eb40658c1
amitbarai <arbarai@hotmail.com>
2019-04-21 11:29:47 +0530

    variable test

--- site/profile/manifests/apache/vh_install.pp
+++ site/profile/manifests/apache/vh_install.pp
@@ -8,16 +8,16 @@ class profile::apache::vh_install (
   {
     file { 'apache_vh_root':
     ensure => directory,
-    path   => $apache_conf_root/$apache_vh_root,
+    path   => "${apache_conf_root}/${apache_vh_root}",
     owner  => root,
     group  => root,
     mode   => '0644',
     }
   -> file_line { 'add_vh_dir_add':
       ensure            => present,
-      path              => $apache_conf_root/conf/httpd.conf,
+      path              => "${apache_conf_root}/conf/httpd.conf",
       after             => '^IncludeOptional\ conf.d/*.conf',
-      match             => '^IncludeOptional\ vh.d/*.conf',
+      match             => "^IncludeOptional\ ${apache_vh_root}/*.conf",
       match_for_absence => true,
     }
 }
