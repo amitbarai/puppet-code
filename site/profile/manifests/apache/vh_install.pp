536db2d093eb658c79cef17803c1d4ff53a7da3a
amitbarai <arbarai@hotmail.com>
2019-04-21 11:17:58 +0530

    spacing

--- site/profile/manifests/apache/vh_install.pp
+++ site/profile/manifests/apache/vh_install.pp
@@ -6,13 +6,13 @@ class profile::apache::vh_install (
   $apache_conf_root = lookup('apache_conf_root')
   )
   {
-  file { 'apache_vh_root':
-  ensure => directory,
-  path   => $apache_conf_root/$apache_vh_root,
-  owner  => root,
-  group  => root,
-  mode   => '0644',
-  }
+    file { 'apache_vh_root':
+    ensure => directory,
+    path   => $apache_conf_root/$apache_vh_root,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    }
   -> file_line { 'add_vh_dir_add':
       ensure            => present,
       path              => $apache_conf_root/conf/httpd.conf,
