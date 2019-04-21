e268e1dd9342be32f286dd9c3b698121fb4b91f7
amitbarai <arbarai@hotmail.com>
2019-04-21 11:12:09 +0530

    include

--- site/profile/manifests/apache/vh_install.pp
+++ site/profile/manifests/apache/vh_install.pp
@@ -3,13 +3,22 @@
 #
 class profile::apache::vh_install (
   $apache_vh_root   = lookup('apache_vh_root'),
+  $apache_conf_root = lookup('apache_conf_root')
   )
   {
   file { 'apache_vh_root':
   ensure => directory,
-  path   => $apache_vh_root,
+  path   => $apache_conf_root/$apache_vh_root,
   owner  => root,
   group  => root,
   mode   => '0644',
   }
+  -> file_line { 'add_vh_dir_add':
+      ensure            => present,
+      path              => $apache_conf_root/conf/httpd.conf,
+      after             => '^IncludeOptional\ conf.d/*.conf',
+      match             => '^IncludeOptional\ vh.d/*.conf',
+      match_for_absence => true,
+    }
 }
+
