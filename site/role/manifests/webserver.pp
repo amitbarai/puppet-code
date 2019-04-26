9165c5ae20885622cddfb18aa94397818cfcd2ed
amitbarai <arbarai@hotmail.com>
2019-04-26 13:11:35 +0530

    hotfix

--- site/role/manifests/webserver.pp
+++ site/role/manifests/webserver.pp
@@ -3,6 +3,6 @@
 #
 class role::webserver {
   contain profile::apache::base_install
-  contain profile::apache::vh_install
-  contain profile::apache::wwwdata_deploy
+  #contain profile::apache::vh_install
+  #contain profile::apache::wwwdata_deploy
 }
