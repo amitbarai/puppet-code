fc8faeb6a00b0c708778cdef63597df52526abcc
amitbarai <arbarai@hotmail.com>
2019-05-02 21:58:10 +0530

    fix

--- site/role/manifests/webserver.pp
+++ site/role/manifests/webserver.pp
@@ -5,5 +5,5 @@ class role::webserver {
   #contain profile::apache::base_install
   #contain profile::apache::vh_install
   #contain profile::apache::wwwdata_deploy
-  contain profile::tomcat::base_install
+  contain profile::tomcat::tom_install
 }
