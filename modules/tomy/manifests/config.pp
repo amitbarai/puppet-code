918536b78b9ca0d7ec7d49d59fa42c8fe85e438d
amitbarai <arbarai@hotmail.com>
2019-09-12 01:16:07 +0530

    teting

--- modules/tomy/manifests/config.pp
+++ modules/tomy/manifests/config.pp
@@ -54,7 +54,7 @@ file { $tomy_conf_file:
   owner   => $tomcatuser,
   group   => $tomcatgroup,
   mode    => '0755',
-  content => template('tomy/tomcat.conf.erb')
+  content => template('tomy/tomcat.conf.epp')
 }
 
 #  Changing ports in server.xml file
